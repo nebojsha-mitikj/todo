0ca52fc924caca6381746a9d26ce3a43706ba60b
Nebojsha <nebojsha.97.mitikj@gmail.com>
2023-06-30 22:10:53 +0200

    [main] Add a register link to the login page.

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -77,10 +77,17 @@ const submit = () => {
             </div>
 
             <div class="flex items-center justify-end mt-4">
+
+                <Link :href="route('register')"
+                      class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                    Register
+                </Link>
+
                 <Link
                     v-if="canResetPassword"
                     :href="route('password.request')"
-                    class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    class="underline ml-4 text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
                     Forgot your password?
                 </Link>
