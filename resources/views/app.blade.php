c2e17486040697eee3f2350994b9b1d02932c5be
Nebojsha <nebojsha.97.mitikj@gmail.com>
2024-04-20 12:37:35 +0200

    [main] Upgrade insecure requests

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,12 +2,10 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title inertia>{{ config('app.name', 'Todo') }}</title>
-
         <link rel="icon" href="{{ asset('images/n.png') }}" type="image/png">
-
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
